df9449aaef77448edb1ce3aeb5cff219fd56a737
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-03-10 12:28:54 +0100

    disable openmp on os x for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 autoreconf --install
-./configure --prefix="${PREFIX}"
+
+if [ `uname` == Darwin ]; then
+	USE_OPENMP=false
+else
+	USE_OPENMP=true
+fi;
+
+./configure --prefix="${PREFIX}" --enable-openmp=$USE_OPENMP
 
 make -j${CPU_COUNT}
 make install
